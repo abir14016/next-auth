7e333c9cc75026aa372358f9306b06891b47f400
abir14016 <abir14016@gmail.com>
2023-08-22 21:51:08 +0600

    cloned starter pack for nextJS project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
+//starter pack cloned (ant design included)
 const HomePage = () => {
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>Welcome To Next Auth Home Page</h1>
     </div>
   );
 };
