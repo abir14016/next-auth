7bb83e6626127128585620ad3a1b3cb5e7374b49
abir14016 <abir14016@gmail.com>
2023-08-23 13:38:36 +0600

    email password authentication done with firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCvdDRyzCLDPLk1sAlLYJys3R8vD5EIohY",
+    authDomain: "watch-gallary-98d9a.firebaseapp.com",
+    projectId: "watch-gallary-98d9a",
+    storageBucket: "watch-gallary-98d9a.appspot.com",
+    messagingSenderId: "567312501468",
+    appId: "1:567312501468:web:343306345bf252ea754219"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import Head from "next/head";
 import { useSession } from "next-auth/react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "@/firebase/firebase.auth";
 const HomePage = () => {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
+  const [user, loading, error] = useAuthState(auth);
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
       <h1 style={{ textAlign: "center", marginTop: "10%" }}>Welcome To Next Auth Home Page</h1>
-      <h2 style={{ textAlign: "center" }}>Logged in user: {session?.user?.name}</h2>
-      <h4 style={{ textAlign: "center" }}>Logged in user Email: {session?.user?.email}</h4>
+      {/* <h2 style={{ textAlign: "center" }}>Logged in user: {session?.user?.name}</h2> */}
+      {
+        session?.user && <h4 style={{ textAlign: "center" }}>Logged in user Email: {session?.user?.email}</h4>
+      }
+      {
+        user?.email && <h4 style={{ textAlign: "center" }}>Logged in user Email: {user?.email}</h4>
+      }
     </div>
   );
 };
